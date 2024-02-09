cfb2b709db4fc70dcdac414ba06ce47942ee3686
Aniket <aniketdhokane111@gmail.com>
2024-02-09 23:18:31 +0530

    mycommit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B b= new B();
-        b.meth();
+        System.out.println(b.meth());
+        System.out.println(b.meth());
     }
 
 
