ecd171df4f24726f0eabb88d2ef6725e26245015
Aniket <aniketdhokane111@gmail.com>
2024-02-09 23:04:56 +0530

    mycommit1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,14 +10,14 @@ public class Main {
 
 }
  class A {
-    public String meth() {
+  String meth() {
 
         return "Invoking method from class A";
     }
 }
 class B extends A{
 
-    public String meth()  { return "Method is overridden in Extendend class B";}
+    String meth()  { return "Method is overridden in Extendend class B";}
 }
 
 
